722c0242bc511aace62adf769852490a84810210
ffinkdevs <fink@h0st.space>
2024-10-15 13:39:51 +0200

    add git to tabby

--- nixos/modules/services/misc/tabby.nix
+++ nixos/modules/services/misc/tabby.nix
@@ -139,7 +139,7 @@ in {
   config = lib.mkIf cfg.enable {
     environment = {
       # etc."tabby/config.toml".source = format.generate "config.toml" cfg.settings;
-      systemPackages = [tabbyPackage];
+      systemPackages = [tabbyPackage pkgs.git];
     };
 
     systemd = let
